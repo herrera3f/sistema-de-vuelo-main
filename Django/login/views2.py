a48fa3625c85c83bbd22c578872013947d91eb37
bimce <benjamin.martinez29@inacapmail.cl>
2023-11-08 18:06:38 -0300

    Create views2.py

---
+++ Django/login/views2.py
@@ -0,0 +1,23 @@
+import pika
+from django.http import JsonResponse
+
+def send_api_request(request):
+    if request.method == 'POST':
+        data = {
+            'username': request.POST['username'],
+            'password': request.POST['password'],
+            # Otras datos necesarios para la solicitud a la API en Node.js
+        }
+        message = json.dumps(data)
+
+        # Publica el mensaje en la cola de RabbitMQ
+        channel.basic_publish(exchange='',
+                              routing_key='api_request_queue',
+                              body=message)
+        return JsonResponse({'message': 'Solicitud enviada a la API en Node.js'})
+
+# Define la ruta en Django
+urlpatterns = [
+    path('send_api_request/', send_api_request, name='send_api_request'),
+    # Otras rutas
+]
